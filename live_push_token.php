84a21e62593b6284dc7ed39a6fd5d5ad7bf20120
monkey-wenjun <admin@fangwenjun.com>
2016-10-12 11:11:11 +0800

    push 2 files

--- live_push_token.php
+++ live_push_token.php
@@ -1,19 +1,23 @@
 <?php
 //填写你的推流域名
-$domain =  'test.v0.live.com';
+$domain =  $_POST["domain"];
 //填写你的app名称
-$live = 'live'; 
+$live = $_POST["live"];
 //填写你的流名
-$stream = 'test'; 
+$stream = $_POST["stream"];
 $rtmp_url = 'rtmp://'.$domain.'/'.$live.'/'.$stream;
 //后台设置的token密钥
-$secret = 'F8BKOKLWBMryIeHSiLqe2gVVDCjCXCoC'; 
+$secret = $_POST["secret"];
 //有效时间，unix时间戳，必须大于当前的时间
-$valid_ts = time()+60; 
+$valid_ts = time()+60+$_POST["valid_ts"]; 
 //过期时间
-$expired_ts = time()+10000000; 
+$expired_ts = time()+86400+$_POST["expired_ts"]; 
 //计算token
 $token = md5($domain.'/'.$live.'/'.$stream.$valid_ts.$expired_ts.$secret); 
-//拼接url
+
 $url = $rtmp_url.'?'.'domain='.$domain.'&'.'token='.$token.'&valid_ts='.$valid_ts.'&expired_ts='.$expired_ts;
-echo 'url:'.$url;
+echo '您计算的token为:'.$url;
+echo '<br>';
+echo '您输入的有效时间戳为:'.$valid_ts;
+echo '<br>';
+echo '您输入的有效时间戳为:'.$expired_ts;
